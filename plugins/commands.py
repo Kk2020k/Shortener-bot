7f495c880cf5b2255310fa3e3e0bf061e416664b
Kk2020k <108136211+Kk2020k@users.noreply.github.com>
2023-01-01 14:18:15 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -247,7 +247,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('Main Channel ', url='https://t.me/MainChannel2k')] 
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('Main Channel ', url='https://t.me/MainChannel2k') ] ] ), 
         protect_content=True if pre == 'filep' else False,
         )
                     
