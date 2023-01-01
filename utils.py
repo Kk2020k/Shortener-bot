d9e79652f8d5602fbe878704e98da334fe8126bb
Kk2020k <108136211+Kk2020k@users.noreply.github.com>
2023-01-01 15:27:49 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -383,7 +383,7 @@ async def get_shortlink(link):
         https = "https"
         link = link.replace("http", https)
 
-    url = f'https://dulink.in/api'
+    url = f'https://cac.teckypress.in/api'
     params = {'api': URL_SHORTNER_WEBSITE_API,
               'url': link,
               }
